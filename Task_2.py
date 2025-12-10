6121fa945520cda18e5d96253ed83b0883497df8
Sneha Deshmukh <sneha.biocode@gmail.com>
2025-12-10 18:14:37 +0530

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your first name: ")
-sir_name = input("Enter your last name: ")
-full_name = nam +" "+ sir_nam
-print("Hello, " + full_name + " ! " + "Welcome to the Python program.")
